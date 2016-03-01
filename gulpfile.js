d9f11822eddf7e6c5b65c70a981b3d6f7058f1be
yaroslav-dyn <norman_zp@outlook.com>
2016-03-01 18:07:16 +0200

    010316

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+
+	var browserSync = require('browser-sync').create();
+
+	
+// Static Server + watching css/html files
+gulp.task('serv', function() {
+
+    browserSync.init({
+        server: "./"
+    });
+    gulp.watch("*.html" ).on('change', browserSync.reload);
+    gulp.watch("css/*.css" ).on('change', browserSync.reload);
+});
+
+gulp.task('watch', function(){
+	gulp.watch('css/*.css');
+})
+
+
+
