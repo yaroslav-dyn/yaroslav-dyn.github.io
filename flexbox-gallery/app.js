6749681348b332084aa193c9174fd5fb342d2c5d
yaroslav-dyn <norman_zp@outlook.com>
2016-04-18 18:54:15 +0300

    add jq plugin alfa

--- flexbox-gallery/app.js
+++ flexbox-gallery/app.js
@@ -10,44 +10,44 @@ var app = angular.module('galleryApp', []);
 	var imgGems = [
 		{
 			"itemHeadline": "Item 1",
-			"itemImgSrc": "http://lorempixel.com/640/480/abstract/",
+			"itemImgSrc": "img/img-3.jpg",
 			"itemDescription": "Description 1 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat doloremque, debitis possimus expedita ea atque."
 		},
 		{
 			"itemHeadline": "Item 2",
-			"itemImgSrc": "http://lorempixel.com/640/480/abstract/1",
+			"itemImgSrc":"img/img-4.jpg",
 			"itemDescription": "Description 2 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus at eius expedita cum, quaerat dolore?"
 		},
 		{
 			"itemHeadline": "Item 3",
-			"itemImgSrc": "http://lorempixel.com/640/480/abstract/3",
+			"itemImgSrc": "img/img-5.jpg",
 			"itemDescription": "Description 3 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat doloremque, debitis possimus expedita ea atque."
 		},
 		{
 			"itemHeadline": "Item 4",
-			"itemImgSrc": "http://lorempixel.com/640/480/abstract/5",
+			"itemImgSrc": "img/img-6.jpg",
 			"itemDescription": "Description 4 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus at eius expedita cum, quaerat dolore?"
 		},
 				{
 			"itemHeadline": "Item 5",
-			"itemImgSrc": "http://lorempixel.com/640/480/abstract/10",
+			"itemImgSrc": "img/img-7.jpg",
 			"itemDescription": "Description 5 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat doloremque, debitis possimus expedita ea atque."
 		},
 		{
 			"itemHeadline": "Item 6",
-			"itemImgSrc": "http://lorempixel.com/640/480/abstract/2",
+			"itemImgSrc": "img/img-8.jpg",
 			"itemDescription": "Description 6 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus at eius expedita cum, quaerat dolore?"
 		},
 		{
 			"itemHeadline": "Item 7",
-			"itemImgSrc": "http://lorempixel.com/640/480/abstract/4",
+			"itemImgSrc": "img/img-9.jpg",
 			"itemDescription": "Description 7 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat doloremque, debitis possimus expedita ea atque."
 		},
 		{
 			"itemHeadline": "Item 8",
-			"itemImgSrc": "http://lorempixel.com/640/480/abstract/7",
+			"itemImgSrc": "img/img-2.jpg",
 			"itemDescription": "Description 8 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus at eius expedita cum, quaerat dolore?"
-		},
+		}
 	];
 
 })();
--- flexbox-gallery/mvc.js
+++ flexbox-gallery/mvc.js
@@ -2,19 +2,57 @@
 //jQuery
 //Modal script
 $(document).ready(function(){
-	$('.img-wrap img, .close-button').on('click', function(){
-		modalView();
-		getBg();
+
+(function( $ ) {
+  $.fn.flexGallery = function(options) {
+
+  	var settings = $.extend({
+  		buttonOpen:false,
+  		addModal: false,
+  		classButton: ''
+  	},options);
+  	return this.each(function(){
+  		if(settings.buttonOpen == true){
+  			var thisClass = settings.classButton;
+ 			$(this).find('.item .advance-rule').append("<a class='a-btn'>Open</a>").addClass(thisClass); 
+  		}	
+  		if(settings.addModal == true){
+  			goModal();
+  		}	
+  	});
+    
+  };
+
+})(jQuery);//end plugin init
+
+	function goModal(){
+		$('.img-wrap img, .close-button').on('click', function(){
 		var srcAttr = $(this).attr('src');
 		$('.modal-content img').attr('src', srcAttr);
+			getBg();
+			modalView();	
 	});
+	}
 	var getBg = function(){
+		backrelEl = 
 		$('.modal-backrel').toggleClass('modal-backrel-view');
 	}
 	var modalView = function(){
 		var modalEl = $('.modal');
-		modalEl.toggleClass('hidden');
-	}	
-});
+		modalEl.fadeToggle(150, function(){
+			$(this).toggleClass('hidden');
+		})
+	};
+
+
+	$('.container-flex').flexGallery({
+		addModal: true,
+		buttonOpen: true,
+		classButton:'button-fg-block'
+	});
+
+
+});//end ready
 //Equalheights for item if a need
 // $('.item').equalHeights();
+
