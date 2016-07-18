1cd96ac9aac4e8a148e00a3249eedf19a54bc390
yaroslav-dyn <norman_zp@outlook.com>
2016-07-18 13:00:50 +0300

    add sky

---
+++ parallax-scroll/scripts/main.js
@@ -0,0 +1,29 @@
+$('#scene-top').parallax({
+    calibrateX: false,
+    calibrateY: true,
+    invertX: false,
+    invertY: true,
+    limitX: false,
+    limitY: 10,
+    scalarX: 2,
+    scalarY: 8,
+    frictionX: 0.2,
+    frictionY: 0.8,
+    originX: 0.0,
+    originY: 1.0
+});
+
+$('#scene-bottom').parallax({
+    calibrateX: false,
+    calibrateY: true,
+    invertX: true,
+    invertY: true,
+    limitX: false,
+    limitY: 10,
+    scalarX: 2,
+    scalarY: 8,
+    frictionX: 0.2,
+    frictionY: 0.8,
+    originX: 0.0,
+    originY: 1.0
+});
